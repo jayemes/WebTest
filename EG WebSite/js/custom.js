8f5bba7a723a253e42978f7c519336b9b7d2e740
JMS <jmscasso@gmail.com>
2016-07-04 21:48:13 -0300

    de todito

--- EG WebSite/js/custom.js
+++ EG WebSite/js/custom.js
@@ -1,9 +1,15 @@
 
-
-sayHello = function(){window.alert("Hello")}	
-
 $(document).ready(function(){
-	$(".icon").css("opacity","0");
 	$(".icon").animate({opacity: 1},1000);
 })
 
+var $root = $('html, body');
+$('a[href*=#]').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+});
