8fe62212b3914a100a9cd05214ff6ecb1dec9d5e
JMS <jmscasso@gmail.com>
2016-07-10 20:24:54 -0300

    c

--- EG WebSite/js/custom.js
+++ EG WebSite/js/custom.js
@@ -1,15 +1,35 @@
+$(document).load(function() {
+    $("#main-container").css("display", "none");
 
-$(document).ready(function(){
-	$(".icon").animate({opacity: 1},1000);
 })
 
+$(document).ready(function() {
+    $(".fixed-up").css("display", "none");
+    $("#main-container").fadeIn(500);
+    $(".icon").animate({ opacity: 1 }, 1000);
+});
+
+// Smooth scrolling on links within the same page
 var $root = $('html, body');
+
 $('a[href*=#]').click(function() {
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top
-    }, 500, function () {
+    }, 500, function() {
         window.location.hash = href;
     });
     return false;
+});
+
+// Up botton appearing when scrolling down
+
+var fixedUp = $(".fixed-up");
+
+$(document).scroll(function () {
+    if (window.pageYOffset > 400) {
+        fixedUp.fadeIn(250);
+    } else {
+        fixedUp.fadeOut(250);
+    }
 });
