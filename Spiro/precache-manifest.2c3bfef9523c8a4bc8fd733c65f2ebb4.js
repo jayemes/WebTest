5d92f293630f0a94610ccea4bfe3502c6a1147c8
Juan Manuel Scasso <jmscasso@gmail.com>
2020-08-11 22:05:28 -0300

    Spiro v0.1

---
+++ Spiro/precache-manifest.2c3bfef9523c8a4bc8fd733c65f2ebb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fd122ebedd1ccf81a6d96c0bd5b658",
+    "url": "/Spiro/index.html"
+  },
+  {
+    "revision": "5bcd2d80c3b50e37b757",
+    "url": "/Spiro/static/css/main.a6ab082c.chunk.css"
+  },
+  {
+    "revision": "5e5381d3be81b861ecf0",
+    "url": "/Spiro/static/js/2.9bfad867.chunk.js"
+  },
+  {
+    "revision": "e7692d061f2ab012502e82e26f4a46c8",
+    "url": "/Spiro/static/js/2.9bfad867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcd2d80c3b50e37b757",
+    "url": "/Spiro/static/js/main.83ac6ed4.chunk.js"
+  },
+  {
+    "revision": "7965b3d87a08b59502d8",
+    "url": "/Spiro/static/js/runtime-main.1991f01b.js"
+  },
+  {
+    "revision": "bca12a0f298b12f203d12e31d85d257f",
+    "url": "/Spiro/static/media/help-24px.bca12a0f.svg"
+  },
+  {
+    "revision": "08250678fe083be69f3a04851e3c6e63",
+    "url": "/Spiro/static/media/moroccan-flower-dark.08250678.png"
+  },
+  {
+    "revision": "6183e7e4ca884514174d77975d0e61b0",
+    "url": "/Spiro/static/media/moroccan-flower.6183e7e4.png"
+  }
+]);
